b9fe6cb78401934fb716d6d829ba3ef6476ab326
progsystem926 <103824489+progsystem926@users.noreply.github.com>
2024-02-12 22:30:07 +0900

    feat: repository層の実装 #23

---
+++ pkg/domain/repository/post_repository.go
@@ -0,0 +1,8 @@
+package repository
+
+import "github.com/progsystem926/bbs-nextjs-go-back/pkg/domain/model"
+
+type Post interface {
+	GetPosts() ([]*model.Post, error)
+	CreatePost(post *model.Post) (*model.Post, error)
+}
